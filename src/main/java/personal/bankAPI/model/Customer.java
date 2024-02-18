8437ec3e673d8c537c565c9b9d5b418b1217b2b4
roland-liedtke <liedtke.roland@gmail.com>
2024-02-18 21:03:26 +0100

    add app layers and temporary Customer model

---
+++ src/main/java/personal/bankAPI/model/Customer.java
@@ -0,0 +1,10 @@
+package personal.bankAPI.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String pesel;
+}
---
+++ src/main/java/personal/bankAPI/model/Info.java
@@ -0,0 +1,10 @@
+package personal.bankAPI.model;
+
+public class Info {
+    private final static String title = "bankAPI";
+    private final static String version = "1.0.0";
+    private final static String description = "description of API";
+    private final static String contact = "name: Roland Liedtke, url: rolandliedtke.me, email: hello@rolandliedtke.me";
+    private final static Number apiId = 5;
+    
+}
